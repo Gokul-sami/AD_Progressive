ea62953d780340bb2528482abaa6fb40fdb543dd
gokul <gokulpandiyan6@gmail.com>
2024-09-06 21:58:42 +0530

    new changes

--- service-worker.js
+++ service-worker.js
@@ -2,7 +2,7 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('v1').then(cache => {
             return cache.addAll([
-                '/AD_Assign//',
+                '/AD_Assign/',
                 '/AD_Assign/index.html',
                 '/AD_Assign/style.css',
                 '/AD_Assign/images/tadow.jpg'
