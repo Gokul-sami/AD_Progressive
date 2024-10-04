c9f661a6cda1a6dbcab2f45d2cd7491e68608850
gokul <gokulpandiyan6@gmail.com>
2024-10-04 20:36:28 +0530

    new changes

--- service-worker.js
+++ service-worker.js
@@ -24,3 +24,11 @@ self.addEventListener('fetch', event => {
     );
 });
 
+if ('serviceWorker' in navigator) {
+    navigator.serviceWorker.register('/service-worker.js')
+    .then(function(registration) {
+        console.log('Service Worker registered', registration);
+    }).catch(function(error) {
+        console.log('Service Worker registration failed', error);
+    });
+}
