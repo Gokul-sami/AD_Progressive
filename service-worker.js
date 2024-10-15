b21d1e5f58447906f53ffeddfcb564b1805757be
Gokul-sami <gokulpandiyan6@gmail.com>
2024-10-15 18:56:13 +0530

    new changes

--- service-worker.js
+++ service-worker.js
@@ -2,10 +2,10 @@ self.addEventListener('install', event => {
     event.waitUntil(
         caches.open('v1').then(cache => {
             return cache.addAll([
-                'https://gokul-sami.github.io/AD_Asssign/',
-                'https://gokul-sami.github.io/AD_Asssign/index.html',
-                'https://gokul-sami.github.io/AD_Asssign/style.css',
-                'https://gokul-sami.github.io/AD_Asssign/images/tadow.jpg'
+                'https://gokul-sami.github.io/AD_Progressive',
+                'https://gokul-sami.github.io/AD_Progressive/index.html',
+                'https://gokul-sami.github.io/AD_Progressive/style.css',
+                'https://gokul-sami.github.io/AD_Progressive/images/tadow.jpg'
             ]).catch(error => {
                 console.error('Failed to cache resources:', error);
             });
@@ -16,25 +16,16 @@ self.addEventListener('install', event => {
 self.addEventListener('fetch', event => {
     event.respondWith(
         caches.match(event.request)
-        .then(response => response || fetch(event.request))
-        .catch(error => {
+        .then(response => {
+            return response || fetch(event.request).then(fetchResponse => {
+                return caches.open('v1').then(cache => {
+                    cache.put(event.request, fetchResponse.clone());
+                    return fetchResponse;
+                });
+            });
+        }).catch(error => {
             console.error('Fetching failed:', error);
-            // Optionally provide a fallback response
-        })
-    );
-});
-
-self.addEventListener('activate', event => {
-    const cacheWhitelist = ['v1'];
-    event.waitUntil(
-        caches.keys().then(cacheNames => {
-            return Promise.all(
-                cacheNames.map(cacheName => {
-                    if (!cacheWhitelist.includes(cacheName)) {
-                        return caches.delete(cacheName);
-                    }
-                })
-            );
+            // Optionally provide a fallback response here
         })
     );
 });
